e28c0263233ff8ae1ffe5599360941a4a4adeec6
John Kehinde <kehindejohn018@gmail.com>
2023-03-10 13:12:24 +0100

    reading from file

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,6 +1,11 @@
-import * as fs from 'node:fs';
+import * as fs from 'fs';
+//const fs = require('fs');
 
-fs.readFileSync()
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textInput);
 
-const hello = 'Hello World';
-console.log(hello);
+const textOur = `This what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt', textOur);
+console.log('File Written');
+//const hello = 'Hello World';
+//console.log(hello);
