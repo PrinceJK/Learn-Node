1b1efab6fb3e38a801f06f7b778a7f9ecef56413
John Kehinde <kehindejohn018@gmail.com>
2023-03-18 11:19:20 +0100

    Our own modules

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,9 +1,10 @@
 //import * as fs from 'fs';
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
+//const path = require('path');
 const url = require('url');
 //import * as http from 'http';
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////////////////////
 //Files
@@ -35,20 +36,20 @@ const url = require('url');
 
 /////////////////////////////////////////////////////
 //Server
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
+// const replaceTemplate = (temp, product) => {
+//     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//     output = output.replace(/{%IMAGE%}/g, product.image);
+//     output = output.replace(/{%PRICE%}/g, product.price);
+//     output = output.replace(/{%FROM%}/g, product.from);
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//     output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//     if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-    return output;
-}
+//     return output;
+// }
 
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
